76d309b2538660a086eed36f232824265bd67cf8
Dave Cridland <dave.cridland@surevine.com>
2014-05-13 14:53:17 +0100

    Add CryptoUtils wrappers for PBKDF2, SHA-1

---
+++ src/main/java/com/calclab/emite/core/client/xmpp/sasl/CryptoUtils.java
@@ -0,0 +1,27 @@
+package com.calclab.emite.core.client.xmpp.sasl;
+
+import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.crypto.PBEParametersGenerator;
+import org.bouncycastle.crypto.digests.SHA1Digest;
+
+/*
+ * This is PBKDF2 (PKCS#5, RFC 2898), using HMAC and SHA-1.
+ */
+
+public class CryptoUtils {
+	static int SHA1BITS = 160;
+	static public byte[] PBKDF2(byte[] password, byte[] salt, int iterations) {
+		PBEParametersGenerator gen = new PKCS5S2ParametersGenerator();
+		gen.init(password,  salt,  iterations);
+		KeyParameter k = (KeyParameter)gen.generateDerivedParameters(SHA1BITS);
+		return k.getKey();
+	}
+	static public byte[] SHA1(byte[] input) {
+		SHA1Digest h = new SHA1Digest();
+		h.update(input, 0, input.length);
+		byte[] output = new byte[SHA1BITS/8];
+		h.doFinal(output, 0);
+		return output;
+	}
+}
---
+++ src/test/java/com/calclab/emite/core/client/xmpp/sasl/CryptoUtilsTest.java
@@ -0,0 +1,30 @@
+package com.calclab.emite.core.client.xmpp.sasl;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+public class CryptoUtilsTest {
+	// Take a byte[] and produce a hex string of the form found in draft-josefsson-pbkdf2-test-vectors-00, except with an additional terminating space.
+	final static char[] hex = "0123456789abcdef".toCharArray();
+	static String bytesToHex(byte[] bytes) {
+		char[] out = new char[bytes.length * 3];
+		for (int j = 0; j != bytes.length; ++j) {
+			int v = bytes[j] & 0xFF;
+			out[(j * 3)] = hex[v >>> 4];
+			out[(j * 3) + 1] = hex[v & 0x0F];
+			out[(j * 3) + 2] = ' ';
+		}
+		return new String(out);
+	}
+	
+	@Test
+	public void josefssonTestVectors() {
+		String tmp = bytesToHex(CryptoUtils.PBKDF2("password".getBytes(), "salt".getBytes(), 1));
+		assertTrue(tmp.equals("0c 60 c8 0f 96 1f 0e 71 f3 a9 b5 24 af 60 12 06 2f e0 37 a6 "));
+		assertTrue(bytesToHex(CryptoUtils.PBKDF2("password".getBytes(), "salt".getBytes(), 2)).equals("ea 6c 01 4d c7 2d 6f 8c cd 1e d9 2a ce 1d 41 f0 d8 de 89 57 "));
+		assertTrue(bytesToHex(CryptoUtils.PBKDF2("password".getBytes(), "salt".getBytes(), 4096)).equals("4b 00 79 01 b7 65 48 9a be ad 49 d9 26 f7 21 d0 65 a4 29 c1 "));
+		assertTrue(bytesToHex(CryptoUtils.PBKDF2("password".getBytes(), "salt".getBytes(), 16777216)).equals("ee fe 3d 61 cd 4d a4 e4 e9 94 5b 3d 6b a2 15 8c 26 34 e9 84 "));
+		// Above is nightmarishly long to run.
+	}
+}
